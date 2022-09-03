7ff33013c7415b5c98ae1c1c2f547660cd0d4388
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-09-03 22:46:08 +0200

    feat(todos): Redirect to TodoDetails on click on todo

--- src/modules/Todos/modules/TodoTableRow/TodoTableRow.js
+++ src/modules/Todos/modules/TodoTableRow/TodoTableRow.js
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import { useMutation } from '@apollo/client';
 import { UPDATE_TODO_STATUS_BY_ID } from '../../../../gpl-helpers/todo-mutation'
+import { useNavigate } from 'react-router-dom'
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+const StyledTd = styled.td`
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export const TodoTableRow = ({ id, createdAt, type, isDone = false, title }) => {
   // TODO: understand why I did not succeed to use [updateTodoStatus, { loading, data }]
   const [updateTodoStatus] = useMutation(UPDATE_TODO_STATUS_BY_ID);
   const [isChecked, setIsChecked] = useState(isDone)
+  const navigate = useNavigate()
 
   const handleChange = async (e) => {
     e.preventDefault()
@@ -14,11 +24,14 @@ export const TodoTableRow = ({ id, createdAt, type, isDone = false, title }) => 
     const { updateTodoStatusById } = fetchResult.data
     setIsChecked(updateTodoStatusById.isDone)
   }
+
+  const handleClick = () => navigate(`/todo/${id}`)
+
   return (
     <tr>
-      <td>{title}</td>
-      <td>{type}</td>
-      <td>{createdAt}</td>
+      <StyledTd onClick={handleClick} css={css``}>{title}</StyledTd>
+      <StyledTd onClick={handleClick}>{type}</StyledTd>
+      <StyledTd onClick={handleClick}>{createdAt}</StyledTd>
       <td>
         <input
           type="checkbox"
